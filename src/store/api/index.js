b49705bf14a6fe7d5e98a78af3ec3c4e51d5fb81
Admin <mike@Mikes-MacBook-Air.local>
2022-12-21 23:31:02 +0700

    api

--- src/store/api/index.js
+++ src/store/api/index.js
@@ -1,5 +1,5 @@
 //BASE URL
-export const BASE_URL = "http://127.0.0.1:8000";
+export const BASE_URL = "http://tugasakhir.libraryclusteringkmeans.web.id";
 
 //Api Sample Data
 export const API = "/api";
